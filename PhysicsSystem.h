8a7bf23ca687e8a419c1b04d451fb5d013ccfa9a
Hadrnn <timvasilenko2002@gmail.com>
2023-05-13 17:45:57 +0300

    Initial inputs

--- PhysicsSystem.h
+++ PhysicsSystem.h
@@ -2,12 +2,13 @@
 #include "System.h"
 #include "Types.h"
 #include "Entity.h"
+#include "Input.h"
 #include <SFML/Graphics.hpp>
 
 class PhysicsSystem : public System {
 public:
 
-    void updatePositions(std::vector<bool> inputs, const sf::Mouse& mouse) {
+    void updatePositions(const Input& inputs) {
 
         /*HealthComponent* temp_component = dynamic_cast<HealthComponent*>(scene[1].getComponentByID(ComponentID::HealthComponent));
         temp_component->setHealth(10);*/
