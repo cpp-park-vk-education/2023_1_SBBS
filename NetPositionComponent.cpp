a2590c8a66eafc84567bd9d09d0b8766036b4ab3
Hadrnn <timvasilenko2002@gmail.com>
2023-05-31 23:17:15 +0300

    Fixed inf while x2

--- NetPositionComponent.cpp
+++ NetPositionComponent.cpp
@@ -9,25 +9,29 @@ void NetTankPositionComponent::update(sf::RenderWindow& window, std::vector<Enti
     Position new_position = new_component.getPosition();
     package from_net_position = NetConnector::getInstance().get();
 
+    int current_entity_id = scene[i]->getEntityID();
+
     while (from_net_position) {
         if (from_net_position.eventType_ != TANK_POSITION_MARK) {
             from_net_position = NetConnector::getInstance().get();
         }
+        else {
+            if (current_entity_id == from_net_position.id_) {
+                new_position.x = from_net_position.info1_;
+                new_position.y = from_net_position.info2_;
+                new_position.rotation = from_net_position.info3_;
+
+                CollisionComponent* my_collision = dynamic_cast<CollisionComponent*>(scene[i]->getComponentByID(ComponentID::CollisionComponent));
+                if (my_collision) { /// проверяем наличие компоненты только для гарантии безопасности исключений 
+                    CollisionComponent new_collision = *my_collision;
+                    new_collision.update(new_position, new_position.rotation);
+                    *my_collision = new_collision;
+                }
+                setPosition(new_position);
+                break;
+            }
+        }
     }
-
-
-    new_position.x = from_net_position.info1_;
-    new_position.y = from_net_position.info2_;
-    new_position.rotation = from_net_position.info3_;
-
-    /// 
-    CollisionComponent* my_collision = dynamic_cast<CollisionComponent*>(scene[i]->getComponentByID(ComponentID::CollisionComponent));
-    if (my_collision) { /// проверяем наличие компоненты только для гарантии безопасности исключений 
-        CollisionComponent new_collision = *my_collision;
-        new_collision.update(new_position, new_position.rotation);
-        *my_collision = new_collision;
-    }
-    setPosition(new_position);
 }
 
 
@@ -39,23 +43,30 @@ void NetTurretPositionComponent::update(sf::RenderWindow& window, std::vector<En
     Position new_position = new_component.getPosition();
     package from_net_position = NetConnector::getInstance().get();
 
+    int current_entity_id = scene[i]->getEntityID();
+
     while (from_net_position) {
         if (from_net_position.eventType_ != TURRET_POSITION_MARK) {
             from_net_position = NetConnector::getInstance().get();
         }
-    }
+        else {
+            if (current_entity_id == from_net_position.id_) {
 
-    new_position.x = from_net_position.info1_;
-    new_position.y = from_net_position.info2_;
-    new_position.rotation = from_net_position.info3_;
-    
-    CollisionComponent* my_collision = dynamic_cast<CollisionComponent*>(scene[i]->getComponentByID(ComponentID::CollisionComponent));
-    if (my_collision) {
-        CollisionComponent new_collision = *my_collision;
-        new_collision.update(new_position, new_position.rotation);
-        *my_collision = new_collision;
+                new_position.x = from_net_position.info1_;
+                new_position.y = from_net_position.info2_;
+                new_position.rotation = from_net_position.info3_;
+
+                CollisionComponent* my_collision = dynamic_cast<CollisionComponent*>(scene[i]->getComponentByID(ComponentID::CollisionComponent));
+                if (my_collision) {
+                    CollisionComponent new_collision = *my_collision;
+                    new_collision.update(new_position, new_position.rotation);
+                    *my_collision = new_collision;
+                }
+                original_component->setPosition(new_position);
+                break;
+            }
+        }
     }
-    original_component->setPosition(new_position);
 }
 
 void NetBulletPositionComponent::update(sf::RenderWindow& window, std::vector<Entity*>& scene, int& i) {
@@ -64,22 +75,27 @@ void NetBulletPositionComponent::update(sf::RenderWindow& window, std::vector<En
     PositionComponent new_component = *original_component;
     Position new_position = new_component.getPosition();
     package from_net_position = NetConnector::getInstance().get();
+    int current_entity_id = scene[i]->getEntityID();
 
     while (from_net_position) {
         if (from_net_position.eventType_ != BULLET_POSITION_MARK) {
             from_net_position = NetConnector::getInstance().get();
         }
-    }
+        else {
+            if (current_entity_id == from_net_position.id_) {
 
-    new_position.x = from_net_position.info1_;
-    new_position.y = from_net_position.info2_;
-    new_position.rotation = from_net_position.info3_;
-    /// 
-    CollisionComponent* my_collision = dynamic_cast<CollisionComponent*>(scene[i]->getComponentByID(ComponentID::CollisionComponent));
-    if (my_collision) {
-        CollisionComponent new_collision = *my_collision;
-        new_collision.update(new_position, new_position.rotation);
-        *my_collision = new_collision;
+                new_position.x = from_net_position.info1_;
+                new_position.y = from_net_position.info2_;
+                new_position.rotation = from_net_position.info3_;
+                /// 
+                CollisionComponent* my_collision = dynamic_cast<CollisionComponent*>(scene[i]->getComponentByID(ComponentID::CollisionComponent));
+                if (my_collision) {
+                    CollisionComponent new_collision = *my_collision;
+                    new_collision.update(new_position, new_position.rotation);
+                    *my_collision = new_collision;
+                }
+                original_component->setPosition(new_position);
+            }
+        }
     }
-    original_component->setPosition(new_position);
 }
