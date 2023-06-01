d4e10f8a8b9732705112540cf4698fcb923fa03f
Hadrnn <timvasilenko2002@gmail.com>
2023-06-01 03:18:47 +0300

    Fixed bullet spawn

--- SpawnerSystem.cpp
+++ SpawnerSystem.cpp
@@ -17,29 +17,14 @@ int SpawnerSystem::update(sf::RenderWindow& window, std::vector<Entity*>& scene)
 
 			break;
 		case BULLET_SPAWN_EVENT:
-			bs.Spawn(Position(net_data.info1_, net_data.info2_, net_data.info3_), net_data.info4_);
+			scene.push_back(bs.Spawn(Position(net_data.info1_ + 100, net_data.info2_ + 100, net_data.info3_), net_data.info4_));
 			break;
 
 		default:
 			break;
 		}
 		net_data = NetConnector::getInstance().getEvent();
-	}
-
-
-		//if (net_data == BULLET_SPAWN_EVENT) {
-		//	Position bullet_position;
-		//	bullet_position.x = net_data[1];
-		//	bullet_position.y = net_data[2];
-		//	bullet_position.rotation = net_data[3];
-		//	//char type = net_data[j+4];
-		//	char type = '1'; ///////////////////нормально передать тип
-
-		//	scene.push_back(bs.Spawn(bullet_position, type));
-		//}
-
-		//net_data = NetConnector::getInstance().get();
-	
+	}	
 
 	return 0;
 }
